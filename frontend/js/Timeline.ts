ce906d12013d928468fb1a601a17d0b7a53df2db
Pavlos <pavlos.evangelatos@student.uva.nl>
2020-03-09 10:44:18 +0100

    barchart

--- frontend/js/Timeline.ts
+++ frontend/js/Timeline.ts
@@ -1,3 +1,4 @@
+import data from './data_bar.json'
 import Configuration from './Configuration'
 import ServerAPI from './ServerAPI'
 import BasicD3Visualization from './BasicD3Visualization'
@@ -8,58 +9,78 @@ export default class Timeline extends BasicD3Visualization
   {
 
     super('timeline');
-
-    let ctx = this;
+    
+    
+   // let ctx = this;
     // we get the data from the Flask server
     // the data is return as a JSON and can be used inmediately
     // the server has to be running
-    (ServerAPI.get()).getData(function(data) {
+
+    //(ServerAPI.get()).getData(function(data) {
     // append the svg object to the body of the page
+    //d3.json("http://localhost:5000/main", function( data) {
 
-    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json", function( data) {
-      // Initialize the links
-      var link = ctx.svg
-        .selectAll("line")
-        .data(data.links)
-        .enter()
-        .append("line")
-        .style("stroke", "#aaa")
+    
 
-      // Initialize the nodes
-      var node = ctx.svg
-        .selectAll("circle")
-        .data(data.nodes)
-        .enter()
-        .append("circle")
-          .attr("r", 20)
-          .style("fill", "#69b3a2")
+//#####################################################################33
+     var margin = {top: 20, right: 20, bottom: 70, left: 40},
+     width = 600 - margin.left - margin.right,
+     height = 300 - margin.top - margin.bottom;
 
-      // Let's list the force we wanna apply on the network
-      var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
-          .force("link", d3.forceLink()                               // This force provides links between nodes
-                .id(function(d) { return d.id; })                     // This provide  the id of a node
-                .links(data.links)                                    // and this the list of links
-          )
-          .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-          .force("center", d3.forceCenter(ctx.width / 2, ctx.height / 2))     // This force attracts nodes to the center of the svg area
-          .on("end", ticked);
+// Parse the date / time
+    var x = d3.scaleBand()
+          .range([0, width])
+          .padding(0.1);
+    var y = d3.scaleLinear()
+          .range([height, 0]);
+          
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+     .append("g")
+      .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
 
-      // This function is run at each iteration of the force algorithm, updating the nodes position.
-      function ticked() {
-        link
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+// get the data from csv
+    // d3.csv("barchart.csv", function(error, data) {
+    //   if (error) throw error;
 
-        node
-             .attr("cx", function (d) { return d.x+6; })
-             .attr("cy", function(d) { return d.y-6; });
-      }
+     console.log(data)
+    //  console.log(data.volume[0])
+
+  // format the data
+     data.forEach(function(d) {
+        d.volume = +d.volume;
+     });
+
+  // Scale the range of the data in the domains
+      x.domain(data.map(function(d) { return d.threemonths; }));
+      y.domain([0, d3.max(data, function(d) { return d.volume; })]);
+
+  // append the rectangles for the bar chart
+      svg.selectAll(".bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.threemonths); })
+          .attr("width", x.bandwidth())
+          .attr("y", function(d) { return y(d.volume); })
+          .attr("height", function(d) { return height - y(d.volume); });
+
+  // add the x Axis
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+  // add the y Axis
+      svg.append("g")
+          .call(d3.axisLeft(y));
 
     });
-
-});
+ //######################################    
 
   }
 }
