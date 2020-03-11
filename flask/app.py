ea409e9d515c36eddb805373a9ddc7b8dd2b5022
https://github.com/jorijnsmit/dsp.c4 <mideruit@gmail.com>
2020-03-11 13:41:37 +0100

    Merge branch 'master' of https://github.com/MichielDeRuiter/InfoVis-team-4
    # Conflicts:
    #	flask/app.py

--- flask/app.py
+++ flask/app.py
@@ -23,7 +23,7 @@ socketio = SocketIO(app)
 #data = pd.read_csv('../data/reddit_total.csv')
 #data_50 = pd.read_csv('../../data/reddit_total_50.csv')
 data = pd.read_csv('../data/reddit_total_400.csv')
-data_days = data.groupby('days').size().to_frame('volume')
+data_days = data.groupby('days').size().to_frame()
 
 
 main_menu_response = {
@@ -207,7 +207,7 @@ def main_screen():
 		treshold = v[int(top)]
 		start = time.process_time()
 		data2 = data[data.SOURCE_SUBREDDIT.isin(v.index[v.gt(treshold)])]
-	else: 
+	else:
 		data2 = data
 	if (fromDate != None and endDate != None):
 		response = data2.loc[data2['days'].between(int(fromDate), int(endDate))]
@@ -234,26 +234,22 @@ def main_screen():
 			status=200,
 			mimetype="application/json"
 		)
-		
+
 @app.route("/maintotal", methods=['GET', 'POST'])
 @cross_origin()
 def main_screen_total():
 	fromDate, endDate = None, None
 	if 'fromDate' in request.args:
-		fromDate = int(request.args['fromDate'])
+		fromDate = request.args['fromDate']
 	else:
 		fromDate = 0
 	if 'endDate' in request.args:
-		endDate = int(request.args['endDate'])
+		endDate = request.args['endDate']
 	else:
 		endDate = 1216
 	if (fromDate != None and endDate != None):
-		days = []
-		response = data_days[fromDate:endDate].values
+		response = data_days[fromDate:endDate]
 		main_menu_response_filtered = []
-		for i in range(fromDate, endDate):
-			days.append({i:int(response[(i-fromDate)][0])})
-		main_menu_response_filtered = {"timeSeries":days}
 		return app.response_class(
 				response=json.dumps(main_menu_response_filtered),
 				status=200,
@@ -411,9 +407,9 @@ def vis3():
 def vis4():
     return render_template('vis4.html', name='leagueoflegends,soccer')
 
-@app.route('/data_the_avengers.csv')
-def favicon():
-    return redirect(url_for('static', filename='csv/data_the_avengers.csv'))
+# @app.route('/data_the_avengers.csv')
+# def favicon():
+#     return redirect(url_for('static', filename='csv/data_the_avengers.csv'))
 
 @app.route('/plot')
 def plot():
